74bde61f165ea80a365ed15e54dfb7e4bd2af69c
Nandini Anaparti <nandini.anaparti@gmail.com>
2021-03-23 14:16:16 -0500

    Position marker dropped on the map

--- maps - Incomplete/app.js
+++ maps - Incomplete/app.js
@@ -1,20 +1,15 @@
 let form = document.querySelector("form");
 let search = document.querySelector("input");
 let latitude;
-let longitude
+let longitude;
+let marker1;
 let map;
 
 navigator.geolocation.getCurrentPosition(function (position) {
   console.log(position);
   latitude = position.coords.latitude;
   longitude = position.coords.longitude;
-  mapboxgl.accessToken = 'pk.eyJ1IjoibmFuZGluaS1hIiwiYSI6ImNrbW1iN2xqdjFqYmYycG80bmo2bDYwN24ifQ.GQN5FI2XaZYpt8KKxYcMQQ';
-  map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [longitude, latitude], // starting position [lng, lat]
-    zoom: 13 // starting zoom
-  });
+  displayMap();
 }, function error(err) {
   alert(`ERROR(${err.code}): ${err.message}
   DEFAULT LOCATION: (-74.5, 40)` );
@@ -24,5 +19,18 @@ navigator.geolocation.getCurrentPosition(function (position) {
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
     center: [-74.5, 40], // starting position [lng, lat]
     zoom: 9 // starting zoom
-    });
-});
+  });
+}, {enableHighAccuracy: true, timeout: 5000, maximumAge: 0});
+
+function displayMap() {
+  mapboxgl.accessToken = 'pk.eyJ1IjoibmFuZGluaS1hIiwiYSI6ImNrbW1iN2xqdjFqYmYycG80bmo2bDYwN24ifQ.GQN5FI2XaZYpt8KKxYcMQQ';
+  map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [longitude, latitude], // starting position [lng, lat]
+    zoom: 13 // starting zoom
+  });
+  marker1 = new mapboxgl.Marker()
+    .setLngLat([longitude, latitude])
+    .addTo(map);
+}
